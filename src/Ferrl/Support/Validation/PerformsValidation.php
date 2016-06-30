cc5fda3d6d061458b434aefa1ec0713e2acd2759
Lucas Ferreira <lucasfercunha@gmail.com>
2016-06-30 00:52:37 -0300

    Adds easy custom validator trait

---
+++ src/Ferrl/Support/Validation/PerformsValidation.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Ferrl\Support\Validation;
+
+use Illuminate\Contracts\Validation\Factory as Validator;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\MessageBag;
+use Illuminate\Validation\ValidationException;
+
+/**
+ * @property MessageBag errorBag
+ */
+trait PerformsValidation
+{
+    /**
+     * Throws validation errors.
+     *
+     * @throws ValidationException
+     */
+    protected function throwValidationException()
+    {
+        $request = request();
+        $validator = app(Validator::class)->make([], []);
+
+        throw new ValidationException($validator, $this->buildFailedValidationResponse(
+            $request, $this->formatValidationErrors()
+        ));
+    }
+
+    /**
+     * Adds a new error to the error bag.
+     *
+     * @param string $field
+     * @param string $message
+     */
+    protected function addValidationError($field, $message)
+    {
+        $this->errorBag->add($field, $message);
+    }
+
+    /**
+     * Return an error response.
+     *
+     * @param Request $request
+     * @param array $errors
+     * @return Response
+     */
+    protected function buildFailedValidationResponse(Request $request, array $errors)
+    {
+        if (($request->ajax() && ! $request->pjax()) || $request->wantsJson()) {
+            return new JsonResponse($errors, 422);
+        }
+
+        return redirect()->to($this->getRedirectUrl())
+            ->withInput($request->input())
+            ->withErrors($errors, 'default');
+    }
+
+    /**
+     * Return errors as an array.
+     *
+     * @return array
+     */
+    protected function formatValidationErrors()
+    {
+        return $this->errorBag->getMessages();
+    }
+
+    /**
+     * Get previous url link.
+     *
+     * @return string
+     */
+    protected function getRedirectUrl()
+    {
+        return app(UrlGenerator::class)->previous();
+    }
+
+    /**
+     * Return true if there is errors in the bag.
+     *
+     * @return bool
+     */
+    protected function hasErrors()
+    {
+        return !!$this->errorBag->count();
+    }
+}
