0208b29c784edff684af61de631ed8fa7202a8e5
Lucas Ferreira <lucasferreira@mobister.com.br>
2017-01-02 11:49:28 -0300

    Adds missing middleware for flash messages

---
+++ tests/Ferrl/Support/Middleware/ShareMessagesFromSession.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Ferrl\Support\Middleware;
+
+use Closure;
+use Illuminate\Contracts\View\Factory as ViewFactory;
+use Illuminate\Support\MessageBag;
+
+class ShareMessagesFromSession
+{
+    /**
+     * The view factory implementation.
+     *
+     * @var \Illuminate\Contracts\View\Factory
+     */
+    protected $view;
+
+    /**
+     * Create a new error binder instance.
+     *
+     * @param  \Illuminate\Contracts\View\Factory $view
+     */
+    public function __construct(ViewFactory $view)
+    {
+        $this->view = $view;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $types = flashes()->getTypes();
+
+        foreach ($types as $type) {
+            $this->view->share(
+                str_plural($type), $request->session()->get('flashes.'.$type) ?: new MessageBag
+            );
+        }
+
+        return $next($request);
+    }
+}
